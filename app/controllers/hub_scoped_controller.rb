d8926ab9913e6115d8f8a95bc626f0ba77fa4e5a
Yevhen Shemet <yevhene@gmail.com>
2015-09-05 20:28:14 +0300

    Hub scoped controller

---
+++ app/controllers/hub_scoped_controller.rb
@@ -0,0 +1,21 @@
+class HubScopedController < ApplicationController
+  helper_method :current_hub
+
+  private
+
+  def current_hub
+    @current_hub ||= Hub.find_by!(path: params[:hub_path])
+  end
+
+  def current_client
+    @current_client ||= current_hub.clients.find_by!(user: current_user)
+  end
+
+  def current_vendor
+    @current_vendor ||= current_hub.vendors.find_by!(user: current_user)
+  end
+
+  def default_url_options(*)
+    { hub_path: current_hub.path }
+  end
+end
