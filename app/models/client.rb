4667f5cb0c0b46146988085c40fba2f024135034
Pavel Druziak <pavel.druzyak@gmail.com>
2015-09-19 20:01:07 +0300

    Add order policy

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ActiveRecord::Base
   belongs_to :user
   belongs_to :organization
+  has_many :orders
 
   validates :user, :organization, presence: true
   validates :user_id, uniqueness: { scope: [:organization_id] }
